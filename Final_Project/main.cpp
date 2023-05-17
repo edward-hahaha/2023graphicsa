98f9d3488e29d8f09b17e5d78844173a51901078
edward-hahaha <edward920606@gmail.com>
2023-05-17 12:14:27 +0800

    week14_移動關節

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,88 +1,115 @@
 #include <stdio.h>
 #include <GL/glut.h>
-#include "glm.h"
-GLMmodel * head = NULL;
-GLMmodel * body = NULL;
-GLMmodel * uparmR = NULL;
-GLMmodel * lowarmR = NULL;
-int show[4] = {0,0,1,0};
-int ID=2;
-void keyboard(unsigned char key, int x, int y) {
-    if(key=='0') ID=0;
-    if(key=='1') ID=1;
-    if(key=='2') ID=2;
-    if(key=='3') ID=3;
+#include "glm.h"///week13-1
+GLMmodel * head =NULL;///week13-1
+GLMmodel * uparmR =NULL;///week13-1
+GLMmodel * lowarmR =NULL;///week13-1
+GLMmodel * body =NULL;///week13-1
+int show[4]={1,1,1,1};///week13-3///week14 3-2
+int ID = 3;///week14-3-1 //0頭 1身體 2上手臂 3下手臂///week14 3-2
+void keyboard(unsigned char key,int x, int y){
+    if(key=='0') ID = 0;///week14 3-1
+    if(key=='1') ID = 1;///week14 3-1
+    if(key=='2') ID = 2;///week14 3-1
+    if(key=='3') ID = 3;///week14 3-1
+    ///if(key=='0') show[0] = !show[0];///week13-3
+    ///if(key=='1') show[1] = !show[1];
+    ///if(key=='2') show[2] = !show[2];
+    ///if(key=='3') show[3] = !show[3];
     glutPostRedisplay();
-}
-FILE * fout = NULL;
-FILE * fin = NULL;
-float teapotX=0, teapotY=0;
-float angle=0, angle2=0, angle3=0;
+}///原來的keyboard先註解
+FILE * fout=NULL;///step2-2
+FILE * fin =NULL;///2
+float teapotX=0,teapotY=0;
+float angle=0,angle2=0,angle3=0;
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-        glScalef(0.2, 0.2, 0.2);
-        if(body==NULL){
-            head = glmReadOBJ("model/head.obj");
-            body = glmReadOBJ("model/body.obj");
-            uparmR = glmReadOBJ("model/uparmR.obj");
-            lowarmR = glmReadOBJ("model/lowarmR.obj");
-
+        glScalef(0.2,0.2,0.2);///week13-2
+        if(body==NULL){///week13-1
+            head = glmReadOBJ("model/head.obj");///week13-2
+            body = glmReadOBJ("model/body.obj");///week13-1
+            uparmR = glmReadOBJ("model/uparmR.obj");///week13-3
+            lowarmR = glmReadOBJ("model/lowarmR.obj");///week13-3
+            ///glmUnitize(body);///week13-1
         }
-        if(ID==0)glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if(show[0]) glmDraw(head, GLM_MATERIAL);
-        if(ID==1)glColor3f(0,1,0);
-        else glColor3f(1,1,1);
-        if(show[1]) glmDraw(body, GLM_MATERIAL);
-        glPushMatrix();
-            glTranslatef(-1.200000,+0.453333,0);
-            glRotatef(angle,0,0,1);
-            glTranslatef(1.2000000,-0.453333,0);
+        if(ID==0)glColor3f(1,0,0);///week14-3-1
+        else glColor3f(1,1,1);///week14-3-1
+        if(show[0]) glmDraw(head, GLM_MATERIAL);///week13-1
 
-            if(ID==2)glColor3f(1,0,0);
-            else glColor3f(1,1,1);
-            if(show[2]) glmDraw(uparmR, GLM_MATERIAL);
-        glPopMatrix();
-        if(ID==3)glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);
+        if(ID==1)glColor3f(1,0,0);///week14-3-1
+        else glColor3f(1,1,1);///week14-3-1
+        if(show[1]) glmDraw(body, GLM_MATERIAL);///week13-2
+
+        glPushMatrix();///week13-4
+            glTranslatef(-1.200356,+0.466667,0);///week14-3-2
+            //glTranslatef(teapotX,teapotY,0);
+            glRotatef(angle,0,0,1);///week14-3-2 TRT建出來
+            glTranslatef(1.200356,-0.466667,0);///week14-3-2
+            if(ID==2)glColor3f(1,0,0);///week14-3-1
+            else glColor3f(1,1,1);///week14-3-1
+            if(show[2]) glmDraw(uparmR, GLM_MATERIAL);///week13-3
+
+            glPushMatrix();
+                glTranslatef(-1.946666,0.126667,0);///幫忙找出TRT移動的值///week14-3-3
+                glRotatef(angle,0,0,1);///week14-3-2 TRT建出來
+                glTranslatef(1.946666,-0.126667,0);///week14-3-2
+
+                if(ID==3)glColor3f(1,0,0);///week14-3-1
+                else glColor3f(1,1,1);///week14-3-1
+                if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);///week13-3
+            glPopMatrix();
+
+        glPopMatrix();///week13-4
+
+
     glPopMatrix();
-    glColor3f(0,1,0);
-    glutSolidTeapot(0.02);
+
+    glColor3f(0,1,0);///week14-3-2放個小茶壺在中心當參考點
+    glutSolidTeapot(0.02);///week14-3-2
     glutSwapBuffers();
 }
-int oldX = 0, oldY = 0;
-void motion(int x, int y){
-    teapotX += (x - oldX)/150.0;
-    teapotY -= (y - oldY)/150.0;
+int oldX=0,oldY=0;///week13-4
+void motion (int x,int y){///week13-4
+    teapotX += (x-oldX)/150.0;
+    teapotY-= (y-oldY)/150.0;
     oldX = x;
     oldY = y;
-    angle = x;
-    printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
+    angle= x;///week14-3-2
+    printf("glTranslatef(%f,%f,0);\n",teapotX,teapotY);
     glutPostRedisplay();
 }
-void mouse(int button, int state, int x, int y)
+void mouse(int button,int state,int x,int y)
 {
     if(state==GLUT_DOWN){
-        oldX = x;
-        oldY = y;
-        angle = x;
+        oldX = x;///week13-4teapotX = (x-150)/150.0;
+        oldY = y;///week13-4teapotY = (150-y)/150.0;
+        angle =x;///week13-4
+        ///if(fout==NULL)fout =fopen("file4.txt","w");week12
+        ///fprintf(fout,"%f %f\n",teapotX,teapotY);
     }
     display();
 }
-
-int main(int argc, char** argv)
+//void keyboard(unsigned char key ,int x,int y)///2keyboard函式
+//{
+//    if(fin==NULL){///2如果檔案還沒fopen(),就開他
+//        fclose(fout);///2前面的mouse會開fout 指標,所以要關掉
+//        fin = fopen("file4.txt","r");///2開檔
+//    }
+//    fscanf(fin,"%f %f",&teapotX,&teapotY);///2讀檔
+//    display();///2重畫畫面
+//}
+int main(int argc, char *argv[])
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week12");
+	glutInit(&argc, argv);
+	glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("week14");
 
-    glutDisplayFunc(display);
-    glutMouseFunc(mouse);
-    glutMotionFunc(motion);
-    glutKeyboardFunc(keyboard);
+	glutDisplayFunc(display);
+	glutMouseFunc(mouse);
+	glutMotionFunc(motion);///week13-4
+	glutKeyboardFunc(keyboard);///2keboard要做事了
 
     glutMainLoop();
 }
